4284cc1f2b315d59213b83b20ef86b72fcd74968
hezretpirliyev13 <122713004+hezretpirliyev13@users.noreply.github.com>
2026-05-02 11:45:40 +0400

    Refactor handler functions for clarity and comments

--- input_capture_mode_handlers.h
+++ input_capture_mode_handlers.h
@@ -4,16 +4,16 @@
 #include "variables.h"
 
 void handler_channel_1(void) {
-  if (0b1 & GPIOA->IDR >> 0) {
-    channel_1_start = TIM2->CCR1;
-    TIM2->CCER |= TIM_CCER_CC1P;
-  } else {
-    channel_1 = TIM2->CCR1 - channel_1_start;
-    if (channel_1 < 0) channel_1 += 0xFFFF;
-    TIM2->CCER &= ~TIM_CCER_CC1P;
+  if (0b1 & GPIOA->IDR >> 0) {                //PA0 is HIGH?
+    channel_1_start = TIM2->CCR1;             //Assigning TIM2 Counter value to the channel_1_start
+    TIM2->CCER |= TIM_CCER_CC1P;              //Capture/compare enable reg is CC1 is rising edge
+  } else {                                    //PA0 is LOW
+    channel_1 = TIM2->CCR1 - channel_1_start;  //counter value-channel_1_start = 
+    if (channel_1 < 0) channel_1 += 0xFFFF;    //when counter reaches 65535 then it will reset and channel_1 will be minus value and we need to add 65 535
+    TIM2->CCER &= ~TIM_CCER_CC1P;              //capture/compare reg is cc1 is falling edge
   }
 }
-void handler_channel_2(void) {
+void handler_channel_2(void) {                  //PA1
   if (0b1 & GPIOA->IDR >> 1) {
     channel_2_start = TIM2->CCR2;
     TIM2->CCER |= TIM_CCER_CC2P;
@@ -23,7 +23,7 @@ void handler_channel_2(void) {
     TIM2->CCER &= ~TIM_CCER_CC2P;
   }
 }
-void handler_channel_3(void) {
+void handler_channel_3(void) {                  //PA2
   if (0b1 & GPIOA->IDR >> 2) {
     channel_3_start = TIM2->CCR3;
     TIM2->CCER |= TIM_CCER_CC3P;
@@ -33,7 +33,7 @@ void handler_channel_3(void) {
     TIM2->CCER &= ~TIM_CCER_CC3P;
   }
 }
-void handler_channel_4(void) {
+void handler_channel_4(void) {                    //PA3
   if (0b1 & GPIOA->IDR >> 3) {
     channel_4_start = TIM2->CCR4;
     TIM2->CCER |= TIM_CCER_CC4P;
